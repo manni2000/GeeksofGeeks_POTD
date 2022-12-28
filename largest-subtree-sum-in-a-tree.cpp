0067dc050e150dfd24cb491d7fc0dd7661a6e93f
Manish kumar <91480902+manni2000@users.noreply.github.com>
2022-12-28 17:46:53 +0530

    Update

---
+++ largest-subtree-sum-in-a-tree.cpp
@@ -0,0 +1,17 @@
+int subtree(Node *root, int &ans){
+          if(root==NULL) return 0;
+          
+          int lsum = subtree(root->left, ans);
+          int rsum = subtree(root->right, ans);
+          int tot =  root->data+lsum+rsum;
+          ans = max(ans,tot);
+          return tot;
+      }
+
+    int findLargestSubtreeSum(Node* root)
+    {
+        //Write your code here
+        int ans = INT_MIN;
+        subtree(root, ans);
+        return ans;
+    }
