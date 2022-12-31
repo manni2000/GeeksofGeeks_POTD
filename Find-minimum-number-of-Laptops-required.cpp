5252310c79dfda263e81c8e4dfb557c36a87fb70
Manish kumar <91480902+manni2000@users.noreply.github.com>
2022-12-31 13:58:19 +0530

    Update

---
+++ Find-minimum-number-of-Laptops-required.cpp
@@ -0,0 +1,17 @@
+int minLaptops(int N, int start[], int end[]) {
+        // Code here
+        map<int,int>m;
+        for(int i=0;i<N;i++){
+           m[start[i]]+=1;
+           m[end[i]]-=1;
+        }
+        vector<pair<int,int>>vec;
+        for(auto it: m) vec.push_back(it);
+        
+        int c=0,ans=0;
+        for(pair<int,int>i:vec){
+            c+=i.second;
+            ans=max(ans,c);
+        }
+        return ans;
+    }
