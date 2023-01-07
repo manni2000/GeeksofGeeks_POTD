11870d3fe7bda1f16f393fe6b2cd8030c3242440
Manish kumar <91480902+manni2000@users.noreply.github.com>
2023-01-07 19:36:55 +0530

    Update

---
+++ Flattening a Linked List.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *next;
+    struct Node *bottom;
+
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+        bottom = NULL;
+    }
+};
+
+    Node *flatten(Node *root)
+    {
+        Node *line = root->next;
+        while (line != NULL)
+        {
+            Node *t = line;
+            Node *p = root;
+            Node *p_pre = NULL;
+
+            while (t != NULL && p != NULL)
+            {
+                if (p->data >= t->data && p == root)
+                {
+                    p_pre = t;
+                    t = t->bottom;
+                    p_pre->bottom = p;
+                    root = p_pre;
+                }
+                else if (p->data >= t->data)
+                {
+                    p_pre->bottom = t;
+                    t = t->bottom;
+                    p_pre = p_pre->bottom;
+                    p_pre->bottom = p;
+                }
+                else
+                {
+                    p_pre = p;
+                    p = p->bottom;
+                }
+            }
+            if (p == NULL && t != NULL)
+                p_pre->bottom = t;
+            line = line->next;
+        }
+        return root;
+    }
