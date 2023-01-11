b1ac436a7bee3625e2685d56be626aca93f01e9d
Manish kumar <91480902+manni2000@users.noreply.github.com>
2023-01-11 19:14:17 +0530

    Update

---
+++ Make array elements unique.cpp
@@ -0,0 +1,16 @@
+  #include<bits/stdc++.h>
+  using namespace std;
+  
+  long long int minIncrements(vector<int> arr, int N) {
+        // Code here
+        long long int ans = 0;
+        sort(arr.begin(), arr.end());
+        for(int i=1;i<N;i++){
+            if(arr[i] <= arr[i-1]){
+                int diff = arr[i-1]-arr[i];
+                ans+=diff+1;
+                arr[i]+=diff+1;
+            }
+        }
+        return ans;
+    }
