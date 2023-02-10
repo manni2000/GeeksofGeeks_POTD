81c505ac0389e2a506e3b2c22dc8db0d9a94f055
Manish kumar <91480902+manni2000@users.noreply.github.com>
2023-02-10 19:58:26 +0530

    Update

---
+++ Balloon Everywhere.cpp
@@ -0,0 +1,10 @@
+ int maxInstance(string s){
+        
+        unordered_map<char,int> mp;
+        
+        for(int i=0;i<s.size();i++){
+            mp[s[i]]++;
+        }
+        int a = min({mp['b']/1, mp['a']/1, mp['l']/2, mp['o']/2, mp['n']/1});
+        return a;
+    }
